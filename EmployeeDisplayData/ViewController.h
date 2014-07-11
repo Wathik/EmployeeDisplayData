99461805cd72c7946c2a1cab64af5a11cb10db62
Wathik Almayali <wathikalmayali@Wathik-Almayali.local>
2014-07-11 18:26:34 -0400

    Initial Commit

---
+++ EmployeeDisplayData/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EmployeeDisplayData
+//
+//  Created by Wathik Almayali on 7/11/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EmployeeDisplayDataTests/EmployeeDisplayDataTests.m
@@ -0,0 +1,34 @@
+//
+//  EmployeeDisplayDataTests.m
+//  EmployeeDisplayDataTests
+//
+//  Created by Wathik Almayali on 7/11/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmployeeDisplayDataTests : XCTestCase
+
+@end
+
+@implementation EmployeeDisplayDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
